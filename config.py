20b77e37f9348b5625e2e1cfc04d315b2861817a
k4365 <148364303+k4365@users.noreply.github.com>
2024-02-04 22:50:51 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{quiz game}',
+ version='1.0',
+ author='{k4365}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
